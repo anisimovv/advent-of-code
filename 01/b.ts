a2d1eeace6346ae292177a54b68a5493f0d5ff9b
anisimovv <kirill.anisimovv@gmail.com>
2022-12-01 16:55:57 +0200

    first task done

---
+++ 01/b.ts
@@ -0,0 +1,33 @@
+const input = Deno.readTextFileSync("./input.txt");
+
+const findGroups = (array: string[]): string[][] => {
+  const result = [];
+  let group = [];
+
+  for (const item of array) {
+    if (item.length === 0) {
+      result.push(group);
+      group = [];
+    } else {
+      group.push(item);
+    }
+  }
+
+  return result;
+};
+
+const calories = input.split("\n");
+
+const groupedCallories = findGroups(calories);
+
+const summedGroups = groupedCallories.map((group) => {
+  return group.reduce((acc, item) => {
+    return acc + +item;
+  }, 0);
+});
+
+const sortedGroups = summedGroups.sort((a, b) => b - a);
+
+const result = sortedGroups[0] + sortedGroups[1] + sortedGroups[2];
+
+console.log(result);
